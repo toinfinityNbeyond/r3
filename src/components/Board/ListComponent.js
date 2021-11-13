34a53d7bcfe1bb364b41132c5bb573b68abaa22c
cloud <the_1975@naver.com>
2021-11-13 20:19:40 +0900

    .

--- src/components/Board/ListComponent.js
+++ src/components/Board/ListComponent.js
@@ -1,6 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {parse} from "query-string";
 import axios from "axios";
+import {useHistory} from "react-router-dom";
 
 const pageInfoState = {
     count:0,
@@ -24,24 +25,50 @@ const range = (from, to) => {
 //컴포넌트 만들 때 container용 컴포넌트인지 presentation용 컴포넌트인지 고민해야함
 const ListComponent = ({location}) => {
 
+    const [pageInfo, setPageInfo] = useState(pageInfoState)
+    const [reload, setReload] = useState(false)
+
+    const history = useHistory()
+
     //console.log(location) //찍어서 값 잘 받아오는지 확인
     const params = parse(location.search) //?page=3 이런거 뽑아오기
 
     const pageDTO = {page:params.page | 1, size:params.size | 10} //page 값이 없으면 1, size 값이 없으면 10으로 디폴드 값 주기
 
+    console.log(pageDTO)
+
     useEffect(() => {
+
+        console.log("get json data")
         axios.get(`http://localhost:8080/api/board/list?page=${pageDTO.page}&size=${pageDTO.size}`)
             .then(response => {
-                const data = response.data
-                console.log(data)
+                console.log(response.data)
+                setPageInfo(response.data)
             })
-    },[pageDTO])
 
-    console.log(params) //키:값으로 뽑히는지 로그 확인
+    },[pageDTO.page, reload])
+
+    const dtoLi = pageInfo.dtoList.map(dto => <li key={dto.bno}>{dto.bno} -- {dto.title} </li>)
+
+    const movePage = (pageNum) => {
+        // alert("pageNum: " + pageNum)
+        setReload(!reload)
+        history.push(`/board/list?page=${pageNum}`)
+    }
+
+    const pageLi = range(pageInfo.start, pageInfo.end).map(pageNum =>
+        <li key={pageNum} onClick={() => movePage(pageNum)} >{pageNum}</li>)
+    // console.log(params) //키:값으로 뽑히는지 로그 확인
 
     return (
         <div>
             <h1>Board List Component</h1>
+            <ul>
+                {dtoLi}
+            </ul>
+            <ul>
+                {pageLi}
+            </ul>
         </div>
     );
 };
