5c55e9cb6cbc7c623d22a64038c2168b3c539523
cloud <the_1975@naver.com>
2021-11-13 20:18:36 +0900

    .

--- src/pages/Board/index.js
+++ src/pages/Board/index.js
@@ -1,12 +1,14 @@
 //인덱스 만드는 잉 -> 컴포넌트 많을 때 구분이 쉽기 위해서
 import {Route, Switch} from "react-router-dom";
 import BoardListPage from "./BoardListPage";
+import BoardRegisterPage from "./BoardRegisterPage";
 
 export default () => {
     return (
         <Switch>
             <Route path={"/board/list"} component={BoardListPage}></Route>
             {/* /board로 들어오면 무조건 리스트 떠야함 -> 밖에 전체 index.js에도 코드 추가 */}
+            <Route path={"/board/register"} component={BoardRegisterPage}></Route>
             <Route path={"/board/"} component={BoardListPage}></Route>
         </Switch>
     )
